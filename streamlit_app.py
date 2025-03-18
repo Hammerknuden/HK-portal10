ea5a76b3ab2a198fdb9178b7bbfc2acf61d8631d
Finn <finnjorg@>
2025-03-18 16:33:00 +0100

    update with 2026

--- streamlit_app.py
+++ streamlit_app.py
@@ -158,6 +158,68 @@ if year == '2025':
     print(unique_values)
     st.markdown(f"**Antal ledige rum**  {ledige_rum}")  # "ledige} rum ", {ledige_rum})
 
+if year == '2026':
+
+    if bruger == "naja":
+        df = pd.read_excel(r"C:\Users\naja\OneDrive\DELE MAPPE NAJA\HAMMERKNUDEN\BOOKING\2025_BOOKING_ 2_0.xlsx",
+                           sheet_name='book_simp')
+    if bruger == "finn":
+        df = pd.read_excel(r'C:\Users\finnj\OneDrive\DELE MAPPE NAJA\HAMMERKNUDEN\BOOKING\2025_BOOKING_ 2_0.xlsx',
+                           sheet_name='book_simp')
+
+    else:
+        streamlit.markdown("Fil fejl 2025")
+
+    new_data = df[(df['dato'].dt.date >= checkin_date) & (df['dato'].dt.date < checkout_date)]
+    unique_values = new_data["1-I"].unique()
+
+    counts_1 = new_data["1-I"].value_counts()
+    counts_2 = new_data["2-I"].value_counts()
+    counts_3 = new_data["3-I"].value_counts()
+    counts_4 = new_data["4-I"].value_counts()
+    counts_5 = new_data["5-I"].value_counts()
+    #chat
+    print(f"Counts 1: {counts_1}")
+    print(f"Counts 2: {counts_2}")
+    print(f"Counts 3: {counts_3}")
+    print(f"Counts 4: {counts_4}")
+    print(f"Counts 5: {counts_5}")
+
+    room_1 = (counts_1.get("va", 0))
+    room_2 = (counts_2.get("va", 0))
+    room_3 = (counts_3.get("va", 0))
+    room_4 = (counts_4.get("va", 0))
+    room_5 = (counts_5.get("va", 0))
+    # chat
+    print(f"Room 1: {room_1}")
+    print(f"Room 2: {room_2}")
+    print(f"Room 3: {room_3}")
+    print(f"Room 4: {room_4}")
+    print(f"Room 5: {room_5}")
+
+    if room_1 == days.days:
+        ledige_rum_1 = 1
+    else:
+        ledige_rum_1 = 0
+    if room_2 == days.days:
+        ledige_rum_2 = 1
+    else:
+        ledige_rum_2 = 0
+    if room_3 == days.days:
+        ledige_rum_3 = 1
+    else:
+        ledige_rum_3 = 0
+    if room_4 == days.days:
+        ledige_rum_4 = 1
+    else:
+        ledige_rum_4 = 0
+    if room_5 == days.days:
+        ledige_rum_5 = 1
+    else:
+        ledige_rum_5 = 0
+    ledige_rum = ledige_rum_1 + ledige_rum_2 + ledige_rum_3 + ledige_rum_4 + ledige_rum_5
+    print(unique_values)
+    st.markdown(f"**Antal ledige rum**  {ledige_rum}")  # "ledige} rum ", {ledige_rum})
 
 def highlight_cells(val):
 #chat
