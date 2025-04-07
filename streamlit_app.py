b1177c16e49936286e1678cdffacb36edb487923
Finn <finnjorg@>
2025-04-07 16:53:21 +0200

    known back to D/ running 7apr1653

--- streamlit_app.py
+++ streamlit_app.py
@@ -435,19 +435,19 @@ nationalitet = st.text_input("Nationalitet - DK S N NL etc")
 known_guest = st.checkbox("check for known person")
 if known_guest:
 
-    file_id = '1-9Ks0RY1bMlEM0QuYGVv5_VXztI97Yu6'
-    url = f'https://drive.google.com/uc?id={file_id}'
+    #file_id = '1-9Ks0RY1bMlEM0QuYGVv5_VXztI97Yu6'
+    #url = f'https://drive.google.com/uc?id={file_id}'
     #output = 'database.xlsx'
     #gdown.download(url, output, quiet=False)
-    #df = pd.read_excel("D:\Hammerdata\Database hammerknuden.xlsx", sheet_name="Dtb", dtype={"telefon": str})
+    df = pd.read_excel("D:\Hammerdata\Database hammerknuden.xlsx", sheet_name="Dtb", dtype={"telefon": str})
     #df = pd.read_excel(output, sheet_name='Dtb')
-    df = pd.read_excel(url, sheet_name='Dtb')
+    #df = pd.read_excel(url, sheet_name='Dtb')
     search_value = telefon
-    pd.set_option("display.max_columns", None)
+    pd.set_option("display.max_columns", None,)
     rows1 = df[df['telefon'] == search_value]
-    #df = pd.read_excel("D:\Hammerdata\Database hammerknuden.xlsx", sheet_name="Dtb", dtype={"email": str})
+    df = pd.read_excel("D:\Hammerdata\Database hammerknuden.xlsx", sheet_name="Dtb", dtype={"email": str})
     search_value = email_address
-    df = pd.read_excel(url)
+    #df = pd.read_excel(url)
     pd.set_option("display.max_columns", None)
     rows2 = df[df['Email'] == search_value]
 
