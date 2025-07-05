d8ead1ee248d6e6d1dc435e6c8e37358a93c66a8
Finn <finnjorg@>
2025-07-05 09:34:40 +0200

    excel og mail up and running 5 jul 0934

--- streamlit_app.py
+++ streamlit_app.py
@@ -424,7 +424,7 @@ if known_guest:
     file_id = '1-9Ks0RY1bMlEM0QuYGVv5_VXztI97Yu6'
     url = f'https://drive.google.com/uc?id={file_id}'
 
-    df = pd.read_excel(url, sheet_name='Dtb', dtype={'familienavn': str})
+    df = pd.read_excel(url, sheet_name='Dtb', dtype={'Familienavn': str})
     search_value = fam_name
     pd.set_option("display.max_columns", None, )
     rows1 = df[df['Familienavn'] == search_value]
