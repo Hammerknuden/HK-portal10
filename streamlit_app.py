dc3679304651917e2bc47ba3eebcd9b0475676e1
Finn <finnjorg@>
2025-11-16 14:49:52 +0100

    requirements txt added

--- streamlit_app.py
+++ streamlit_app.py
@@ -618,7 +618,8 @@ if send_data and year == '2026':
                           {single_room}, {breakfast}, {formatted_pristotal}, {known})
         add_data(some_book_data, year, booking_number, name, checkin_date, checkout_date, now, nationalitet, web, seng,
                  rabat, num_rooms, num_guests, email_address, telefon, spouse, single_room, breakfast,
-                 formatted_pristotal, known, comments, excel_path='https://docs.google.com/spreadsheets/d/145-hYZvfkUMGAHI8fP_M6xNVXN-6fpHI/edit?usp=drive_link&ouid=113417285199087461988&rtpof=true&sd=true')
+                 formatted_pristotal, known, comments, excel_path='https://bonneviedisk.de6.quickconnect.to/fbdownload/output26.xlsx?dlink=%222f48616d6d65726b6e7564656e20646174612f6f757470757432362e786c7378%22&noCache=1763300812393&mode=download&stdhtml=false&SynoToken=ARev9TKHrSgEU&SynoHash=c8LsrImQYaXzl7We15UXIXTTSuoUXQ.ODQ')
+                 #'https://docs.google.com/spreadsheets/d/145-hYZvfkUMGAHI8fP_M6xNVXN-6fpHI/edit?usp=drive_link&ouid=113417285199087461988&rtpof=true&sd=true')
                  #excel_path=r"C:\Users\finnj\OneDrive\DELE MAPPE NAJA\HAMMERKNUDEN\BOOKING\filer\2024 output.xlsx")
 
     if bruger == "naja":
