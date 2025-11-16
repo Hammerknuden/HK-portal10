f40d1d1f1f4a8e1c72d0ec6d29571a9591e2e480
Finn <finnjorg@>
2025-11-16 14:58:57 +0100

    requirements txt added

--- excel_database.py
+++ excel_database.py
@@ -13,11 +13,6 @@ def add_data(year, book_data, booking_number, name, checkin_date, checkout_date,
                 'ankomst': '', 'bed': [seng], 'rabat': [procent], 'antal værelser': [num_rooms],
                 'nr gæst': [num_guests], 'Email': [email_address], 'telefon': [telefon], 'Spouse': [spouse],
                 'enkelt': [single_room], 'morgenmad': [BF], 'pris ialt': [pristotal], 'known': [known], 'Comments':[comments]}
-
-    #file_id = '1QGGa7LG9OfryfefhJ4QohGYxqjNJ-fPg'
-    #url = f'https://drive.google.com/uc?id={file_id}'
-    #url = 'https://drive.usercontent.google.com/download?id=1QGGa7LG9OfryfefhJ4QohGYxqjNJ-fPg&export=download&authuser=0&confirm=t&uuid=4ece34a8-c6d3-4fb2-b14f-d679260a2777&at=APcmpoxYX9pwMTHWhzO7j9CamHva:1746354652387'
-    #file = "rC:\Users\finnj\OneDrive\DELE MAPPE NAJA\HAMMERKNUDEN\BOOKING\filer\2025 output.xlsx"
     df1 = pd.DataFrame(book_data)
     print(df1)
     rek = int(booking_number)
