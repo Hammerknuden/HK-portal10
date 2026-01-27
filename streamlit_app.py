e22b32f4e5d1eb6346f20c904c0f39fbc5499f1e
Finn <finnjorg@>
2026-01-27 13:46:56 +0100

    single room update

--- streamlit_app.py
+++ streamlit_app.py
@@ -431,18 +431,18 @@ if year == '2028':
         high_season_price = 1050   #2026 1075
         low_season_price = 930     #2026 950
         single_room = "N"
-if year == '2026':
-    if single_room:
+if year == '2026' and single_room:
+
         high_season_price = 975  #2025 950 ca 3% =985
         low_season_price = 850   #2025 830 ca 4% =865
         single_room = "Y"
-    if web == "FM":
-        high_season_price = 1075
-        low_season_price = 1075
-    else:
-        high_season_price = 1075   #2025 1050 ca 3% = 1085
-        low_season_price = 950     #2025 930 ca 3% = 965
-        single_room = "N"
+        if year == '2026' and web == 'FM':
+            high_season_price = 1075
+            low_season_price = 1075
+        else:
+            high_season_price = 1075   #2025 1050 ca 3% = 1085
+            low_season_price = 950     #2025 930 ca 3% = 965
+            single_room = "N"
 
 print(high_season_price)
 print(low_season_price)
