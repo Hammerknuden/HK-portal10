a6b56dc94861219b0dd6a408eb764302ab50d89b
Finn <finnjorg@>
2026-02-19 14:14:25 +0100

    send mail update

--- streamlit_app.py
+++ streamlit_app.py
@@ -825,11 +825,13 @@ elif Sprog == "D" and booking_submitted:
 else:
     st.markdown('Booking mail er ikke sendt ')
 
+confirmation_password = st.text_input("Admin kodeord")
 send_data_email = st.button("Send data not in use")
 if send_data_email:
-    to_addr = "finnjorg@mail.dk"
+    to_addr = admin_email
     send_data_email(to_addr, confirmation_password, booking_number, name, checkin_date, checkout_date, num_rooms,
                     num_guests, email_address, telefon, formatted_pristotal)
+
     st.markdown("data mail sendt ")
 else:
     st.markdown("data mail ikke sendt ")
