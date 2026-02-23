f94c19faa918b1bf6b8755373073061234f30122
Finn <finnjorg@>
2026-02-23 14:06:20 +0100

    import data from data email

--- streamlit_app.py
+++ streamlit_app.py
@@ -831,10 +831,9 @@ else:
 
 
 send_data_email = st.checkbox("Data - under construction")
-to_addr_1 = {admin_email}
+to_addr_1 = admin_email
 
 if send_data_email and booking_submitted:
-
     send_data_email(to_addr_1, confirmation_password, booking_number, name, checkin_date, checkout_date, num_rooms,
                     num_guests, email_address, telefon, formatted_pristotal)
 
