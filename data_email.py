f2a7dc3a738476746497607baab962ea747db44a
Finn <finnjorg@>
2026-02-28 13:27:48 +0100

    trail and error attachment

--- data_email.py
+++ data_email.py
@@ -115,7 +115,7 @@ def data_email_html_template(
 
 def send_data_email(to_addr_1, confirmation_password, booking_number, name, checkin_date, checkout_date, num_rooms,
                     now, nationalitet, web, seng, procent, num_guests, email_address, telefon,
-                    formatted_pristotal, df1):
+                    formatted_pristotal, comments, df1):
 
     logo_cid = make_msgid()
     html_content = data_email_html_template(logo_cid[1:-1], booking_number, name, checkin_date, checkout_date,
