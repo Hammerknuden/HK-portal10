1bb827dca4e8c779344df3cd2fec561fa27ed513
Finn <finnjorg@>
2026-03-08 18:43:24 +0100

    retning med chat gbt

--- data_email.py
+++ data_email.py
@@ -19,7 +19,7 @@ logo_path = Path("logo2.jpg")
 def add_data(year, booking_number=None, name=None, checkin_date=None, checkout_date=None, now=None,
              nationalitet=None, web=None, ankomst=None, seng=None, procent=None, num_rooms=None,
              num_guests=None, email_address=None, telefon=None, spouse=None, single_room=None,
-             BF=None, pristotal=None, known=None, comments=None):
+             BF=None, formatted_pristotal=None, known=None, comments=None):
 
     book_data = {
         'book nr': [booking_number],
